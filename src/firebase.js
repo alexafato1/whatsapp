54fb78f4c85d8b9a15d21f4e259493d374d6e223
Alexa fato <alexafato@MacBook-Pro-Alexa.local>
2020-08-23 15:16:02 +0300

    26

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCh-OGP57cyAHjaAl0wpCahl58qqXi5Rkg",
+    authDomain: "whatsapp-clone-6e11a.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-6e11a.firebaseio.com",
+    projectId: "whatsapp-clone-6e11a",
+    storageBucket: "whatsapp-clone-6e11a.appspot.com",
+    messagingSenderId: "1072599155005",
+    appId: "1:1072599155005:web:a9449741b8bd5879b10554",
+    measurementId: "G-09L2WHQ96S"
+  };
